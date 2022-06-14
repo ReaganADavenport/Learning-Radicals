3302b4effee763020a527fe9947750e00dbaf116
Reagan Davenport <reagandavenport@Reagans-MacBook-Air-2.local>
2022-06-14 11:49:14 -0400

    created a data folder to hold the different chapters, created the radical data (1-5)

---
+++ src/Data/radicals.js
@@ -0,0 +1,40 @@
+const radical = {
+    radicalData: [
+        {
+            id: 1,
+            chinese: "人",
+            pinyin: "rén",
+            english: "person"
+
+        },
+
+        {
+            id: 2,
+            chinese: "刀",
+            pinyin: "dāo",
+            english: "knife"
+        },
+
+        {
+            id: 3,
+            chinese: "力",
+            pinyin: "lì",
+            english: "strength"
+        },
+
+        {
+            id: 4,
+            chinese: "又",
+            pinyin: "yòu",
+            english: "right hand, again"
+        },
+
+        {
+            id: 5,
+            chinese: "口",
+            pinyin: "kǒu",
+            english: "strength"
+        }
+
+    ]
+}
