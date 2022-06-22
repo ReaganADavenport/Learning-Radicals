6d274af7d0abfedd0db4e82e7d4fd11176d63bc0
Reagan Davenport <reagan.a.davenport@gmail.com>
2022-06-22 11:52:34 -0400

    added route to eng-to-ch

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './Components/Home/home';
 import Lesson1 from './Components/Lesson1/lesson1-study';
 import Lesson1_Ch_Eng from './Components/Lesson1/lesson1-ch->eng';
 import Lesson1_Ch_Pin from './Components/Lesson1/lesson1-ch->pin';
+import Lesson1_Eng_Ch from './Components/Lesson1/lesson1-eng->ch';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/lesson1/study" element={<Lesson1></Lesson1>}></Route>
           <Route path="/lesson1/ch-to-eng" element={<Lesson1_Ch_Eng></Lesson1_Ch_Eng>}></Route>
           <Route path="/lesson1/ch-to-pin" element={<Lesson1_Ch_Pin></Lesson1_Ch_Pin>}></Route>
+          <Route path="/lesson1/eng-to-ch" element={<Lesson1_Eng_Ch></Lesson1_Eng_Ch>}></Route>
         </Routes>
       </Router>
       
