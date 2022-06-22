34bb0398d3160e40521e0421eadb55502939f2a9
Reagan Davenport <reagan.a.davenport@gmail.com>
2022-06-22 11:35:40 -0400

    created the lesson1-ch-pin page

---
+++ src/Components/Lesson1/lesson1-ch->pin.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './lesson1.css';
+
+import ChToPin_Card from '../Card/ch-to-eng';
+import lesson1 from '../../Data/data1';
+
+
+const Lesson1_Ch_Pin = ({handleClick}) => {
+
+    return(
+        <div className='radical'>
+        <h2>Lesson 1 Radicals - Chinese to Pinyin</h2>
+        <div className='board'>
+
+          {lesson1.lesson1Data.map((item, index) => {
+              return(
+                <ChToPin_Card 
+                  itemChinese={item.chinese} 
+                  itemPinyin={item.pinyin} 
+                  itemEnglish={item.english}
+                  item={item} 
+                  key={index} 
+                  handleClick={handleClick}>
+                </ChToPin_Card>
+              )
+          })}
+        </div>
+
+      </div>
+    )
+}
+
+export default Lesson1_Ch_Pin;
